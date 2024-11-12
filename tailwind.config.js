1813ab22da27d8f5aebaf92c63a5c792bdf7c30c
theo <theospielman@gmail.com>
2024-11-12 15:46:42 -0800

    so many things

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,59 +1,48 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/*.html"],
-  theme: {
-    extend: {
-      colors: {
-        omtbeige: '#edecea',
-        omtgray: '#545454',
-        omtea: '#595649',
-      },
-      fontFamily: {
-        forum: ['forumregular'],
-      },
-      screens: {
-        'sm': '640px',
-        // => @media (min-width: 640px) { ... }
-  
-        'md': '768px',
-        // => @media (min-width: 768px) { ... }
-  
-        'lg': '1024px',
-        // => @media (min-width: 1024px) { ... }
-  
-        'xl': '1280px',
-        // => @media (min-width: 1280px) { ... }
-      }
-    },
-  },
-  variants: {
-    
-  },
-  plugins: [
-    function({ addUtilities }) {
-      const newUtilities = {
-        '.shape-outside-none': {
-          'shape-outside': 'none',
-        },
-        '.shape-outside-circle': {
-          'shape-outside': 'circle(50%)',
-        },
-        '.shape-outside-ellipse': {
-          'shape-outside': 'ellipse(50% 50%)',
-        },
-        '.shape-outside-inset': {
-          'shape-outside': 'inset(10px 10px 10px 10px)',
-        },
-        '.shape-outside-polygon': {
-          'shape-outside': 'polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%)',
-        },
-        '.shape-outside-url': {
-          'shape-outside': 'url(path/to/image.png)',
-        },
-        // Add more custom shapes as needed
-      };
+    content: ["./public/*.html"],
+    theme: {
+        extend: {
+            colors: {
+                omtbeige: "#edecea",
+                omtgray: "#545454",
+                omtea: "#595649",
+            },
+            fontFamily: {
+                forum: ["forumregular"],
+            },
+            screens: {
+                sm: "640px",
+                // => @media (min-width: 640px) { ... }
 
-      addUtilities(newUtilities, ['responsive', 'hover']);
-    }
-  ],
-}
+                md: "768px",
+                // => @media (min-width: 768px) { ... }
+
+                lg: "1024px",
+                // => @media (min-width: 1024px) { ... }
+
+                xl: "1280px",
+                // => @media (min-width: 1280px) { ... }
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+    corePlugins: {
+        preflight: true,
+    },
+    future: {
+        removeDeprecatedGapUtilities: true,
+        purgeLayersByDefault: true,
+        defaultLineHeights: true,
+        standardFontWeights: true,
+    },
+    extend: {
+        float: {
+            right: "float: right",
+            left: "float: left",
+        },
+    },
+};
